2f681809a7960aa46bfb27569edb63095cf9fe34
Akshat Samdani <akshatsamdani@Akshats-MacBook-Pro.local>
2025-01-12 15:45:54 +0530

    Initialize Spring Maven Project

---
+++ src/test/java/com/akshat/springsecuritytelusko/SpringSecurityTeluskoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshat.springsecuritytelusko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTeluskoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
